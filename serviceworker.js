4505ec84e3bb38ad0b66695e0d7174a4787969da
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-05-14 00:26:31 +0300

    Offline mode
    Phew!

---
+++ serviceworker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (event) => {
+	console.log("Installing");
+	event.waitUntil(async function() {
+		let cache = await caches.open("dontcare");
+		cache.addAll(["/", "index.html"]);
+	});
+});
+
+async function handleFetch(event) {
+	try {
+		let response = await fetch(event.request);
+		let cache = await caches.open("dontcare");
+		cache.put(event.request, response.clone());
+		console.log("From network");
+		return response;
+   	} catch (error) {
+		console.log("From cache");
+		return caches.match(event.request);
+	}
+}
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(handleFetch(event));
+});
+
